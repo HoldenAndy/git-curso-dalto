26ba422287fc58111ab056e1e38c61437127d1ae
holdenandy <kaziroz09@gmail.com>
2025-04-13 20:44:03 -0500

    Se agregó una clase Persona y dentro del Main se creó un nuevo objeto con caracteristicas nuevas.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Persona persona1 = new Persona();
+        persona1.setNombre("Andrés Alonso");
+        persona1.setApellido("Carrasco Martínez");
+        persona1.setEdad(20);
+        persona1.setNacionalidad(new String[]{"peruano", "estadounidense"});
+        System.out.println(persona1);
     }
 }
